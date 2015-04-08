1e5faf5d9f2ec50cd88451ad1b33a404b29e7177
Hugues Malphettes <hmalphettes@gmail.com>
2015-04-08 15:48:39 +0100

    Set the sources for node-gyp to avoid patching it.
    Place the sources of nodejs/iojs into ~/.node-gyp/#{version}
    to avoid letting node-gyp download them.
    
    Remove the controversial patching of node-gyp.
    
    Caveat: `node-gyp install` will fail.

--- Formula/iojs.rb
+++ Formula/iojs.rb
@@ -33,14 +33,19 @@ class Iojs < Formula
       ENV["NPM_CONFIG_LOGLEVEL"] = "verbose"
 
       cd buildpath/"npm_install" do
-        # Patch node-gyp until github.com/TooTallNate/node-gyp/pull/564 is resolved
-        # Patch extracted from https://github.com/iojs/io.js/commit/82227f3
-        p = Patch.create(:p1, :DATA)
-        p.path = Pathname.new(__FILE__).expand_path
-        p.apply
         system "./configure", "--prefix=#{libexec}/npm"
         system "make", "install"
       end
+
+      # Pre-download the sources for node-gyp until github.com/TooTallNate/node-gyp/pull/564 is resolved
+      if File.directory?("#{ENV['HOME']}/.node-gyp/#{version}")
+        system "rm", "-rf", "#{ENV['HOME']}/.node-gyp/#{version}"
+      end
+      system "mkdir", "-p", "#{ENV['HOME']}/.node-gyp/#{version}"
+      system "cp", "-a", "#{buildpath}/.", "#{ENV['HOME']}/.node-gyp/#{version}"
+      # mimick node-gyp version install. The number '9' has not changed since June 2012:
+      # https://github.com/TooTallNate/node-gyp/commit/569a9b2b4f55faa4347448058f6c4b2e791c3934
+      File.open("#{ENV['HOME']}/.node-gyp/#{version}/installVersion", 'w') {|f| f.write("9") }
 
       if build.with? "completion"
         bash_completion.install \
@@ -127,27 +132,3 @@ class Iojs < Formula
     end
   end
 end
-
-__END__
-diff --git a/node_modules/node-gyp/lib/install.js b/node_modules/node-gyp/lib/install.js
-index 6f72e6a..ebc4e57 100644
---- a/node_modules/node-gyp/lib/install.js
-+++ b/node_modules/node-gyp/lib/install.js
-@@ -39,7 +39,7 @@ function install (gyp, argv, callback) {
-     }
-   }
-
--  var distUrl = gyp.opts['dist-url'] || gyp.opts.disturl || 'http://nodejs.org/dist'
-+  var distUrl = gyp.opts['dist-url'] || gyp.opts.disturl || 'https://iojs.org/dist'
-
-
-   // Determine which node dev files version we are installing
-@@ -185,7 +185,7 @@ function install (gyp, argv, callback) {
-
-       // now download the node tarball
-       var tarPath = gyp.opts['tarball']
--      var tarballUrl = tarPath ? tarPath : distUrl + '/v' + version + '/node-v' + version + '.tar.gz'
-+      var tarballUrl = tarPath ? tarPath : distUrl + '/v' + version + '/iojs-v' + version + '.tar.gz'
-         , badDownload = false
-         , extractCount = 0
-         , gunzip = zlib.createGunzip()
